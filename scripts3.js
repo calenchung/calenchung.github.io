ece140718f19d1fac86713a54aca9e55ed0ced8f
Calen Chung <calenchung@gmail.com>
2019-09-26 00:26:58 -0400

    yes

--- scripts3.js
+++ scripts3.js
@@ -4,7 +4,7 @@ function playOrPause(){
 
     if(vid.paused){
         vid.play();
-        $("#playBtn").attr("src","pause-btn-01.svg");
+        $("#playBtn").attr("src","pause-btn-02.svg");
     }
     else{
         vid.pause();
@@ -14,14 +14,14 @@ function playOrPause(){
 
 var vid2 = document.getElementById("myVideo2");
 
-function play2Pause(){
+function play3Pause(){
 
     if(vid2.paused){
         vid2.play();
-        $("#playBtn2").attr("src","pause-btn-01.svg");
+        $("#playBtn2").attr("src","pause-btn-020.svg");
     }
     else{
         vid2.pause();
-        $("#playBtn2").attr("src","play-btn-01.svg");
+        $("#playBtn2").attr("src","play-btn-010.svg");
     }
 }
