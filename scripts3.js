a2f68ba9e80d21df11eb2e15d9950d720856a975
Calen Chung <calenchung@gmail.com>
2019-10-03 18:48:25 -0400

    update

--- scripts3.js
+++ scripts3.js
@@ -14,7 +14,7 @@ function playOrPause(){
 
 var vid2 = document.getElementById("myVideo2");
 
-function play3Pause(){
+function play2Pause(){
 
     if(vid2.paused){
         vid2.play();
@@ -22,6 +22,20 @@ function play3Pause(){
     }
     else{
         vid2.pause();
+        $("#playBtn2").attr("src","play-btn-010.svg");
+    }
+}
+
+var vid3 = document.getElementById("myVideo3");
+
+function play3Pause(){
+
+    if(vid3.paused){
+        vid3.play();
+        $("#playBtn2").attr("src","pause-btn-020.svg");
+    }
+    else{
+        vid3.pause();
         $("#playBtn2").attr("src","play-btn-010.svg");
     }
 }
